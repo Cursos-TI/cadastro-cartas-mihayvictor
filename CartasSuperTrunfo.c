d496774d21240854664091a9441a168b3d2965d4
Mihay Victor <111930015+mihayvictor@users.noreply.github.com>
2026-05-31 03:57:21 +0000

    Fiz o desafio nível novato.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,26 +10,26 @@ int main() {
   int populacao_1, n_pontos_turisticos_1;
   float area_1, pib_1;
 
-  char estado_2[20], codigo_carta_2[3], cidade_2[20];
+  /*char estado_2[20], codigo_carta_2[3], cidade_2[20];
   int populacao_2, n_pontos_turisticos_2;
-  float area_2, pib_2;
+  float area_2, pib_2;*/
 
   // Área para entrada de dados
   printf("------- PRIMEIRA CARTA -------\n");
   printf("Digite o nome do estado: ");
-  scanf("%s\n", estado_1);
+  scanf("%s", estado_1);
   printf("Digite o código da carta: ");
-  scanf("%s\n", codigo_carta_1);
+  scanf("%s", codigo_carta_1);
   printf("Digite o nome da cidade: ");
-  scanf("%s\n", cidade_1);
+  scanf("%s", cidade_1);
   printf("Qual o número de habitante da cidade? ");
-  scanf("%i\n", &populacao_1);
+  scanf("%i", &populacao_1);
   printf("Qual a área da cidade em Km²? ");
-  scanf("%f\n", &area_1);
+  scanf("%f", &area_1);
   printf("Qual o PIB da cidade (em bilhões de reais)? ");
-  scanf("%f\n", &pib_1);
+  scanf("%f", &pib_1);
   printf("Quantos pontos turísticos a cidade tem? ");
-  scanf("%i\n", &n_pontos_turisticos_1);
+  scanf("%i", &n_pontos_turisticos_1);
 
 
   // Área para exibição dos dados da cidade
@@ -37,9 +37,9 @@ int main() {
   printf("Estado: %s\n", estado_1);
   printf("Código: %s\n", codigo_carta_1);
   printf("Nome da cidade: %s\n", cidade_1);
-  printf("Área: %.2f", area_1);
-  printf("PIB: %.2f", pib_1);
-  printf("Número de pontos turísticos: %i", n_pontos_turisticos_1);
+  printf("Área: %.2f\n", area_1);
+  printf("PIB: %.2f\n", pib_1);
+  printf("Número de pontos turísticos: %i\n", n_pontos_turisticos_1);
 
 return 0;
 } 
